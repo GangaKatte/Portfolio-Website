da93803dbe7022a08aac68539a825afd1fe4e988
GangaKatte <gangakatte883.com>
2025-04-25 11:33:39 +0530

    all code add

---
+++ script.js
@@ -0,0 +1,9 @@
+//For Animation
+ var typed = new Typed(".text", {
+            strings: ["Full Stack Web Developer", "Python Developer"],
+            typeSpeed: 100,
+            backSpeed: 100,
+            backDelay: 1000,
+            loop: true
+        });
+   
